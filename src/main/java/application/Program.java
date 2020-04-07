3d8a881105def41e15f3f39e87000612ad0403b5
Andressa <andressaszx@gmail.com>
2020-04-07 15:24:13 -0300

    Implementando o vetor inicial

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -5,11 +5,41 @@
  */
 package application;
 
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author Andressa Silva
  */
 public class Program {
     
+    public static void main(String[] args){
     
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter number of products: ");
+        int n = sc.nextInt();
+        
+        Product[] vect = new Product[n];
+        
+        for(int i=0; i<n; i++){
+            sc.nextLine();
+            System.out.printf("Name of product [%d]:  ", i);
+            String name = sc.nextLine();
+            System.out.printf("Price of product [%d]:  ", i);
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+        
+        double sum = 0.0;
+        for(int i=0; i<n; i++){
+            sum+= vect[i].getPrice();
+        }
+        double avg = sum / n;
+        System.out.printf("AVERAGE PRICE: $ %.2f ", avg);
+        sc.close();
+    }
 }
